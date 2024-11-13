ce9b808bf030329624044c8652fa3cad34f7002c
kymin <park48664866@gmail.com>
2024-11-13 11:51:52 +0900

    initialize project

---
+++ src/main/java/com/kwhackathon/broom/BroomApplication.java
@@ -0,0 +1,13 @@
+package com.kwhackathon.broom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroomApplication.class, args);
+	}
+
+}
