f44128f37113112020db89e3045ab0119c7a1674
kymin <park48664866@gmail.com>
2024-11-13 11:21:48 +0900

    initialize project

---
+++ src/test/java/com/kwhackathon/broom/BroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwhackathon.broom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
