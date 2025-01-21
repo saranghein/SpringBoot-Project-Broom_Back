51603c74dac345fd16f68a517cae780343ca6200
kymin <park48664866@gmail.com>
2025-01-21 15:04:47 +0900

    feat: refresh token 쿠키 반환 구현
    refresh token 쿠키 반환 구현:
    - 로그인 시 refresh token 쿠키 반환 구현
    - 로그아웃 시 쿠키 만료 구현
    - 토큰 발급 시 검증 과정 수정

--- src/main/java/com/kwhackathon/broom/common/filter/LogoutFilter.java
+++ src/main/java/com/kwhackathon/broom/common/filter/LogoutFilter.java
@@ -3,13 +3,11 @@ package com.kwhackathon.broom.common.filter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseCookie;
 import org.springframework.web.filter.GenericFilterBean;
 
+import com.kwhackathon.broom.common.util.CookieGenerator;
 import com.kwhackathon.broom.common.util.JwtUtil;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
@@ -22,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LogoutFilter extends GenericFilterBean {
     private final JwtUtil jwtUtil;
+    private final CookieGenerator cookieGenerator;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -32,13 +31,12 @@ public class LogoutFilter extends GenericFilterBean {
     private void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws IOException, ServletException {
         // /logout경로로 post요청을 통해 로그아웃이 호출되었는지 검증
-        String requestUri = request.getRequestURI();
-        if (!requestUri.matches("^\\/logout$")) {
+        if (!request.getRequestURI().matches("^\\/logout$")) {
             filterChain.doFilter(request, response);
             return;
         }
-        String requestMethod = request.getMethod();
-        if (!requestMethod.equals("POST")) {
+        
+        if (!request.getMethod().equals("POST")) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -46,6 +44,8 @@ public class LogoutFilter extends GenericFilterBean {
         // 쿠키에서 refresh토큰 추출
         String refresh = null;
         Cookie[] cookies = request.getCookies();
+
+        // 쿠키 존재 여부 check
         if (cookies == null) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
@@ -57,40 +57,14 @@ public class LogoutFilter extends GenericFilterBean {
             }
         }
 
-        // refresh토큰 null check
-        if (refresh == null) {
+        // refresh토큰 null check, 만료 여부 check
+        if (refresh == null || jwtUtil.isExpired(refresh)) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             return;
         }
 
-        // refresh토큰 만료 여부 check
-        try {
-            jwtUtil.isExpired(refresh);
-        } catch (ExpiredJwtException e) {
-            // response status code
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
-        // 토큰이 refresh인지 확인 (발급시 페이로드에 명시)
-        String category = jwtUtil.getCategory(refresh);
-        if (!category.equals("refresh")) {
-            // response status code
-            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            return;
-        }
-
-        // Refresh 토큰값 null, 생명주기 0인 Cookie 생성
-        // redis나 mysql로 토큰을 관리하는 방법 대신으로 만료 토큰을 바로 만료시켜서 탈취 대응
-        ResponseCookie cookie = ResponseCookie.from("refresh", null)
-                .httpOnly(true)
-                .secure(true)
-                .path("/")
-                .maxAge(0)
-                .sameSite("None")
-                .build();
-
-        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+        response.addCookie(cookieGenerator.generateCookie("refresh", refresh, 0));
+        
         response.setStatus(HttpServletResponse.SC_OK);
 
         response.setCharacterEncoding("UTF-8");
