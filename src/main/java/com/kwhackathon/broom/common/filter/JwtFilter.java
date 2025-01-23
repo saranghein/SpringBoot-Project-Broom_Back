675a2ab56eb17cafa9875b1477094ef85404cf62
kymin <park48664866@gmail.com>
2025-01-23 15:15:18 +0900

    style: 불필요 주석 제거

--- src/main/java/com/kwhackathon/broom/common/filter/JwtFilter.java
+++ src/main/java/com/kwhackathon/broom/common/filter/JwtFilter.java
@@ -13,10 +13,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.kwhackathon.broom.common.util.JwtUtil;
-// import com.kwhackathon.broom.user.entity.User;
-// import com.kwhackathon.broom.user.util.Role;
-
-// import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
