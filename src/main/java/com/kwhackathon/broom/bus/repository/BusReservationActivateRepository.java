82bd38b064cd1e496a1b26afb13d77ef27794455
kymin <park48664866@gmail.com>
2025-03-22 17:02:20 +0900

    fix($BusReservation): 버스 신청 기능 삭제
    버스 신청 기능 삭제:
    - 기획 변경에 따른 버스 신청 기능 삭제

--- src/main/java/com/kwhackathon/broom/bus/repository/BusReservationActivateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kwhackathon.broom.bus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kwhackathon.broom.bus.entity.BusReservationActivate;
-
-public interface BusReservationActivateRepository extends JpaRepository<BusReservationActivate, Long>{
-    
-}
